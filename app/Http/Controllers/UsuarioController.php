2eba4a042966c40b7d02c8a838c05ae675c775d7
Arthur Ramos <arfrra@hotmail.com>
2024-06-29 20:50:21 -0300

    Controle de usuários

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class UsuarioController extends Controller
+{
+    public function login(Request $request) {
+        $retornoUsuario = Usuario::where([
+            'usuario' => $request->usuario,
+            'senha' => $request->senha
+        ])->get();
+
+        if ($retornoUsuario->count() > 0) {
+            $request->session()->put('id', $retornoUsuario->first()->id);
+            return view('index', ['usuario' => $retornoUsuario->first(), 'tela' => 'index']);
+        }
+    }
+
+    public function criar(Request $request) {
+        $retornoUsuario = Usuario::create([
+            'nome' => $request->nome,
+            'documento' => $request->documento,
+            'telefone' => $request->telefone,
+            'usuario' => $request->usuario,
+            'senha' => $request->senha,
+            'cargo' => 1
+        ]);
+
+        if ($retornoUsuario) {
+            return view('login');
+        }
+    }
+
+    public function atualizar(Request $request) {
+        $retornoUsuario = Usuario::where('id', $request->route('id'));
+        $retornoUsuario->update([
+            'nome' => $request->nome,
+            'documento' => $request->documento,
+            'telefone' => $request->telefone,
+            'usuario' => $request->usuario,
+            'senha' => $request->senha,
+            'cargo' => $request->cargo
+        ]);
+
+        return view('index', [
+            'usuario' => Usuario::where('id', $request->session()->get('id'))->get()->first(), 
+            'tela' => 'index'
+        ]);
+    }
+
+    public function telaAtualizar(Request $request) {
+        return view('usuarios.atualizar', [
+            'usuario' => Usuario::where('id', $request->route('id'))->get()->first(),
+            'tela' => 'atualizar'
+        ]);
+    }
+
+    public function mostrar(Request $request) {
+        return view('usuarios.mostrar', [
+            'usuario' => Usuario::where('id', $request->session()->get('id'))->get()->first(),
+            'usuarios' => Usuario::all(),
+            'tela' => 'usuarios'
+        ]);
+    }
+}
---
+++ app/Models/Usuario.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    use HasFactory;
+
+    protected $table = 'usuarios';
+    protected $primaryKey = 'id';
+    protected $connection = 'mysql';
+
+    protected $fillable = [
+        'nome',
+        'documento',
+        'telefone',
+        'usuario',
+        'senha',
+        'cargo'
+    ];
+}
